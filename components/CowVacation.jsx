baf7c473170f0091efd33835eda87ccf09812b32
DK041 <hsenid041@gmail.com>
2025-08-02 17:05:00 +0530

    cows vacation mode

---
+++ components/CowVacation.jsx
@@ -0,0 +1,11 @@
+import "./CowVacation.css";
+export default function CowVacation() {
+  return (
+    <div className="cow-vacation-wrapper">
+      <div className="overlay-content">
+        <h1>Our cows are on vacation 🐄☀️</h1>
+        <p>Milk is temporarily unavailable. We’ll be back soon 🥛</p>
+      </div>
+    </div>
+  );
+}
